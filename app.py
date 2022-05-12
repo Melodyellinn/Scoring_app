a0167abc96eae3c04a2e236325dd3382aa70f291
Melody Migliaccio <welodya@gmail.com>
2022-05-12 14:29:21 +0200

    test_for_app

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ Created on Wed May 11 10:05:45 2022
 @author: Simplon
 """
 
+import pickle
+import pandas as pd
 import streamlit as st
 
+model = pickle.load(open("model.pkl","rb"))
 
+features = pd.DataFrame(
+    model.data, columns= model.feature_names
+)
+#target = pd.Series(model.TARGET)
 
-st.title("Welcome to Streamlit!")
+st.title("Welcome to Dashboard : Score loan")
 
 selectbox = st.sidebar.selectbox(
-    "Select yes or no",
-    ["Yes", "No"]
+    "Select Customers ID",
+    ["model.['SK_ID_CURR'][0]"]
 )
 
 st.write(f"You selected {selectbox}")
