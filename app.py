072f98a30755a17cd7909197917dcef265ef64fd
Melody Migliaccio <welodya@gmail.com>
2022-05-12 14:37:07 +0200

    test_for_app000

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ Created on Wed May 11 10:05:45 2022
 @author: Simplon
 """
 
+import os
 import pickle
-import pandas as pd
 import streamlit as st
 
-model = pickle.load(open("model.pkl","rb"))
 
-features = pd.DataFrame(
-    model.data, columns= model.feature_names
-)
-#target = pd.Series(model.TARGET)
+MODEL_DIR = os.path.join(os.path.dirname('__file__'), 'model.pkl')
+if not os.path.isdir(MODEL_DIR):
+    os.system('runipy notebook_model.ipynb')
+
+model = pickle.load(open("model.pkl","rb"))
+#model = load_model('model.pkl')
 
 st.title("Welcome to Dashboard : Score loan")
 
 selectbox = st.sidebar.selectbox(
     "Select Customers ID",
-    ["model.['SK_ID_CURR'][0]"]
+    ["Yes", "No"]
 )
 
 st.write(f"You selected {selectbox}")
