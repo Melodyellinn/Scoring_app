cdff5db221feb41d66b0679646fd39e1bee9a373
Melody Migliaccio <welodya@gmail.com>
2022-05-13 14:51:08 +0200

    app.py scv path modified

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pickle
 import streamlit as st
 from sklearn.model_selection import train_test_split
 
-path = os.path.join(os.getcwd(), r'..\data\application_train.csv')
+path = os.path.join(os.getcwd(), r'..\data\application_train_light.csv')
 print(path, 'BLOOOOOOOOOOOOOP')
 df = pd.read_csv(path)
 
